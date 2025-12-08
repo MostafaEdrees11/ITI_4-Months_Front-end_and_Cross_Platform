98d3a95648ffcc1a6b6d77a861bb843d2893c6fa
Mostafa Edrees <mostafaedrees018@gmail.com>
2025-12-08 22:31:18 +0200

    feat(Day02 lab of Node JS): implement updateProduct endpoint
    - We implement updateProduct endpoint with some validation.
    - We validate the name & the price of the product.
    - The name & the price here is optional but if the user don't send anyone, this bad request.

--- Node JS/Day 02/Lab Solution/index.js
+++ Node JS/Day 02/Lab Solution/index.js
@@ -42,6 +42,40 @@ server.post('/addProduct', addNewProductValidator, (req, res) => {
     res.status(200).json({msg: "Product added successfully", newProduct});
 });
 
+
+const updateProductValidator = [
+    body("name")
+    .optional()
+    .isString()
+    .withMessage("Product name must be string")
+    .isLength({min: 3, max: 25})
+    .withMessage("Product name must be between 3 and 25 characters"),
+
+    body("price")
+    .optional()
+    .isFloat({ min: 10, max: 1000 })
+    .withMessage("Product price must be number & between 10 and 1000")
+];
+server.put('/updateProduct/:id', updateProductValidator, (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    let productId = +req.params.id;
+    let productIndex = products.findIndex((product) => product.id === productId);
+    if(productIndex === -1) return res.status(404).json({ msg: "Product not found." });
+
+    let {name, price} = req.body;
+    if(!name && !price) return res.status(400).json({ msg: "You don't enter any data" });
+    
+    name = name??products[productIndex].name;
+    price = price??products[productIndex].price;
+    
+    products[productIndex] = {id: productId, name, price};
+    res.status(200).json({msg: "Product updated successfully", product: products[productIndex]})
+});
+
 server.listen(portNum, () => {
     console.log(`server connected on portNum: ${portNum}`);
 })
