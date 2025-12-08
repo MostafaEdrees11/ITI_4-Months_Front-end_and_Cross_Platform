b8ddf9d199a8d8d80997c4b5bf22077d28c31f51
Mostafa Edrees <mostafaedrees018@gmail.com>
2025-12-08 22:29:13 +0200

    feat(Day02 Lab of Node JS): implement addNewProduct endpoint
    - Implement the endpoint & add some validation to it.
    - We validate the name & price of the product.

--- Node JS/Day 02/Lab Solution/index.js
+++ Node JS/Day 02/Lab Solution/index.js
@@ -1,16 +1,46 @@
 const express = require('express');
+const { body, validationResult } = require("express-validator");
 
 const products = require('./products.json');
 
 const server = express();
 const portNum = 8011;
 
+// Middleware to parse incoming JSON requests and puts the parsed data in req.body.
+server.use(express.json());
 
 server.get('/', (req, res) => {
     if(products.length === 0) res.status(200).send('No products yet');
     res.status(200).send(products);
 });
 
+
+const addNewProductValidator = [
+    body("name")
+    .exists()
+    .withMessage("Product name is required")
+    .isString()
+    .withMessage("Product name must be string")
+    .isLength({min: 3, max: 25})
+    .withMessage("Product name must be between 3 and 25 characters"),
+
+    body("price")
+    .exists()
+    .withMessage("Product price is required")
+    .isFloat({ min: 10, max: 1000 })
+    .withMessage("Product price must be number & between 10 and 1000")
+];
+server.post('/addProduct', addNewProductValidator, (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    let newProduct = req.body;
+    newProduct.id = products.length + 1;
+    products.push(newProduct);
+    res.status(200).json({msg: "Product added successfully", newProduct});
+});
 
 server.listen(portNum, () => {
     console.log(`server connected on portNum: ${portNum}`);
