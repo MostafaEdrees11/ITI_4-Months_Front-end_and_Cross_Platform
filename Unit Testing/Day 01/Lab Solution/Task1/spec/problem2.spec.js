666e57a41fbf36b27cd34cbcd5d1131400589e89
Mostafa Edrees <mostafaedrees018@gmail.com>
2026-01-31 13:49:30 +0200

    fix(Unit Testing): fixing the last test case in problem2 in lab1 of unit testing

--- Unit Testing/Day 01/Lab Solution/Task1/spec/problem2.spec.js
+++ Unit Testing/Day 01/Lab Solution/Task1/spec/problem2.spec.js
@@ -2,7 +2,7 @@ const { createArray } = require("../lab1.js");
 
 describe("Test createArray Function", () => {
     it("test that the return value of type array", () => {
-        expect(createArray(7).toEqual(jasmine.any(Array)));
+        expect(createArray(7)).toEqual(jasmine.any(Array));
     })
 
     it("test if we pass 2 it will return array of length 2 and test it includes 1", () => {
@@ -11,9 +11,9 @@ describe("Test createArray Function", () => {
         expect(result).toContain(1);
     })
 
-    it("test if we pass 3 it will return array of length 3 and test it doesn't include 3"), () => {
+    it("test if we pass 3 it will return array of length 3 and test it doesn't include 3", () => {
         const result = createArray(3);
         expect(result.length).toBe(3);
         expect(result).not.toContain(3);
-    }
-})
+    })
+});
